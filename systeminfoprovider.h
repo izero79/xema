a80b05caad96a4d4f5d773018b7f93347a7c8e87
Tero Siironen <izero79@gmail.com>
2012-11-07 23:16:01 +0200

    Systeminfoprovider minimized.

--- systeminfoprovider.h
+++ systeminfoprovider.h
@@ -2,14 +2,9 @@
 #define SYSTEMINFOPROVIDER_H
 
 #include <QObject>
-#include <QVariant>
-#include <QTimer>
-#include <QTime>
-#include <QDateTime>
 
 #ifdef USE_MOBILITY
 #include <QSystemDeviceInfo>
-#include <QSystemNetworkInfo>
 QTM_USE_NAMESPACE
 #endif
 
@@ -22,56 +17,10 @@ public:
 
 signals:
 
-    void batteryLevelChanged( QVariant level );
-    void networkLevelChanged( QVariant level );
-    void wlanLevelChanged( QVariant level );
-    void simStatusChanged( QVariant available );
-    void wlanStatusChanged( QVariant active );
-    void netNameChanged( QVariant active );
-    void timeChanged( QVariant time );
-    void batteryChargingChanged( QVariant charging );
-    void bluetoothStateChanged( QVariant active );
-    void profileChanged( QVariant profile );
-
 public slots:
 
-    void batteryLevelChange( int level );
-    void btStateChanged( bool btOn );
-#ifdef USE_MOBILITY
-    void powerStateChanged( QSystemDeviceInfo::PowerState state );
-    void networkSignalStrengthChanged( QSystemNetworkInfo::NetworkMode mode, int level );
-    void networkStatusChanged(QSystemNetworkInfo::NetworkMode mode, QSystemNetworkInfo::NetworkStatus status );
-    void networkNameChanged( QSystemNetworkInfo::NetworkMode mode, const QString &name );
-    void currentProfileChanged( QSystemDeviceInfo::Profile profile );
-#endif
-
-    int batteryLevel();
-    int networkLevel();
-    int wlanLevel();
-    bool simAvailable();
-    bool wlanActive();
-    bool btActive();
-    QString currentTime();
-    QString networkName();
     static QString versionInfo();
-    static bool touchSupported();
-    static bool landscapeDevice();
-    bool batteryCharging();
-    int currentProfile();
-    static QString currentLanguage();
-    static QString osVersion();
-    static bool qtVersion474();
     static bool imeiAccepted();
-
-    void timeOut();
-
-private:
-
-#ifdef USE_MOBILITY
-    QSystemDeviceInfo *mDeviceInfo;
-    QSystemNetworkInfo *mNetworkInfo;
-#endif
-    QTimer *mClockTimer;
 };
 
 #endif // SYSTEMINFOPROVIDER_H
